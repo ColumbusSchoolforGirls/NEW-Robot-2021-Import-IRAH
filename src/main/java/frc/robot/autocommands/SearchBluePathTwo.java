5f9878744110065ec314d9f8985741b8fd4d33ab
Lina Jeffers <ljeffers@columbusschoolforgirls.org>
2021-02-15 11:34:53 -0500

    adds galactic search paths

---
+++ src/main/java/frc/robot/autocommands/SearchBluePathTwo.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autocommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.ticks;
+import frc.robot.commands.ConveyorManual;
+import frc.robot.commands.ResetGyro;
+import frc.robot.subsystems.ConveyorMotors;
+import frc.robot.subsystems.DriveTrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SearchBluePathTwo extends SequentialCommandGroup {
+  /** Creates a new SearchBluePathTwo. */
+  private final DriveTrain m_drivetrain;
+  private final ConveyorMotors m_conveyor;
+  private final ticks m_ticks;
+
+  public SearchBluePathTwo(DriveTrain drivetrain, ConveyorMotors conveyor, ticks ticks) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    m_drivetrain = drivetrain;
+    m_conveyor = conveyor;
+    m_ticks = ticks;
+    addCommands(
+      new straightforward(ticks.calculateTicks(180), m_drivetrain),
+      new ConveyorManual(1, m_conveyor, true),
+      new ResetGyro(m_drivetrain),
+      new Turn(45, m_drivetrain),
+
+      new straightforward(ticks.calculateTicks(84.853), m_drivetrain),
+      new ConveyorManual(1, m_conveyor, true),
+      new ResetGyro(m_drivetrain),
+      new Turn(-45, m_drivetrain),
+
+      new straightforward(ticks.calculateTicks(180), m_drivetrain),
+      new ConveyorManual(1, m_conveyor, true),
+      new ResetGyro(m_drivetrain),
+      new Turn(135, m_drivetrain),
+      new straightforward(ticks.calculateTicks(60), m_drivetrain)
+      );
+  }
+}
---
+++ src/main/java/frc/robot/autocommands/SearchRedPathOne.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autocommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.ticks;
+import frc.robot.commands.ConveyorManual;
+import frc.robot.commands.ResetGyro;
+import frc.robot.subsystems.ConveyorMotors;
+import frc.robot.subsystems.DriveTrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SearchRedPathOne extends SequentialCommandGroup {
+  /** Creates a new BouncyBoy. */
+
+  private DriveTrain m_drivetrain;
+  private ConveyorMotors m_conveyormotors;
+  private ticks m_ticks;
+
+  public SearchRedPathOne(DriveTrain drivetrain, ConveyorMotors conveyormotors, ticks tick) {
+    m_drivetrain = drivetrain;
+    m_conveyormotors = conveyormotors;
+    m_ticks = tick;
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new straightforward(tick.calculateTicks(90), m_drivetrain),
+      new ConveyorManual(1, m_conveyormotors, true).withTimeout(5),
+      new straightforward(tick.calculateTicks(5), m_drivetrain),
+      new ResetGyro(m_drivetrain),
+      new Turn(-26.5, m_drivetrain),
+
+      new straightforward(tick.calculateTicks(90), m_drivetrain),
+      new ConveyorManual(1, m_conveyormotors, true).withTimeout(5),
+      new straightforward(tick.calculateTicks(5), m_drivetrain),
+      new ResetGyro(m_drivetrain),
+      new Turn(71, m_drivetrain),
+
+      new straightforward(tick.calculateTicks(90), m_drivetrain),
+      new ConveyorManual(1, m_conveyormotors, true).withTimeout(5),
+      new straightforward(tick.calculateTicks(5), m_drivetrain),
+      new ResetGyro(m_drivetrain),
+      new Turn(45.5, m_drivetrain),
+
+      new straightforward(tick.calculateTicks(90), m_drivetrain)
+           
+    );
+  }
+}
